e052d10b605ca32d4d903d5777bf0fcad95aee74
Omar-algassim <omarggg2@gmail.com>
2023-11-28 16:37:13 -0800

    update

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -2,4 +2,4 @@
 # Print all possible combinations of two digits
 for i in range(10):
     for j in range(i+1, 10):
-        print("{}{}".format(i, j), end=", " if j < 89 else "\n")
+        print("{}{}".format(i, j), end=", " if i < 8 else "\n")
