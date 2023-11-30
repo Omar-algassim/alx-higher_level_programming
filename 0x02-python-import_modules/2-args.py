75ca3c8ab4bd6443868b4f5563301b65107365b0
Omar-algassim <omarggg2@gmail.com>
2023-11-30 10:52:21 -0800

    print arguments

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,1 +1,11 @@
 #!/usr/bin/python3
+import sys
+length = len(sys.argv) - 1
+if length >= 1:
+    print("{} arguments:".format(length))
+elif length == 1:
+    print("{} argument:".format(length))
+elif length == 0:
+    print("{} argument.".format(length))
+for i in range (1,length + 1):
+    print("{}:{}".format(i, (sys.argv[i])))
