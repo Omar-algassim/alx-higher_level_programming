5788904a8379dcda3902c28332e4859b298be02d
Omar-algassim <omarggg2@gmail.com>
2023-11-30 11:03:44 -0800

    print arguments

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -7,6 +7,6 @@ if __name__ == "__main__":
     elif length == 1:
         print("{} argument:".format(length))
     elif length == 0:
-        print("{} argument.".format(length))
+        print("{} argument.".format(length), end="")
     for i in range (1,length + 1):
         print("{}:{}".format(i, (sys.argv[i])))
