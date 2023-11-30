68088ff2204337d9598bbee7956d42c130777f31
Omar-algassim <omarggg2@gmail.com>
2023-11-30 12:57:24 -0800

    print module

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
-import hidden_4
-hidden = dir(hidden_4)
-length = len(hidden)
-for i in range(0, length):
-    if hidden[i][0] != '_':
-        print(hidden[i])
+if __name__== "__main__":
+    import hidden_4
+    hidden = dir(hidden_4)
+    length = len(hidden)
+    for i in range(0, length):
+        if hidden[i][0] != '_':
+            print(hidden[i])
