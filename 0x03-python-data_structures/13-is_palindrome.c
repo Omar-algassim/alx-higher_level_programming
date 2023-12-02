6cb54b097eb64a3b8ae5ade90a8f002252920c30
Omar-algassim <omarggg2@gmail.com>
2023-12-02 14:18:46 +0200

    last task

---
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -0,0 +1,52 @@
+#include "lists.h"
+#include <stdlib.h>
+/**
+ * is_palindeome - test if link list is palindrome or not
+ * @head: the head of linked list
+ * Return: 0 if it is not a palindrome, 1 if it is a palindrome
+*/
+
+int is_palindrome(listint_t **head)
+{
+    listint_t *current = *head;
+    int *test;
+    int forward, backward = 0, i = 0;
+
+   if (*head == NULL)
+    {
+        return (0);
+    }
+    while (current->next != NULL)
+    {
+        current = current->next;
+       backward++;
+    }
+    current = *head;
+     test = malloc(sizeof(int) * backward + 1);
+    while (current != NULL)
+    {
+          
+        test[i] = current->n;
+        current = current->next; 
+        i++; 
+    }
+
+    if (i % 2 != 0)
+    {
+        free(test);
+        return (0);
+    }
+
+    forward = backward / 2;
+
+    while (i < forward)
+    {
+        if (test[i] != test[backward])
+        return (0);
+    }
+    free(test);
+    return (1);
+    
+
+
+}
