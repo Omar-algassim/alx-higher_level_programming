47dc7a9563dc31468da8c366a97fe1eb2ea505bb
Omar-algassim <omarggg2@gmail.com>
2023-12-07 04:56:16 +0200

    update_dictionary

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,14 +1,22 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
     sample = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    if not roman_string:
+        return 0
     list(roman_string)
     length = len(roman_string)
+    i = 0
     count = 0
-    for i in range(length):
-        if i + 1 < length - 1:
+    while i < length:
+        if length == 1:
+            count = sample[roman_string[i]]
+            break
+        if i + 1 <= length - 1:
             if sample[roman_string[i]] < sample[roman_string[i + 1]]:
-                count += (sample[roman_string[i]] - sample[roman_string[i + 1]])
-                i + 1
-                print(i)
-        count += sample[roman_string[i]]
+                count = (sample[roman_string[i + 1]] - sample[roman_string[i]])
+                i += 1
+            else:
+                count = sample[roman_string[i]]
+        count += count
+        i += 1
     return count
--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -3,5 +3,5 @@ def update_dictionary(a_dictionary, key, value):
     if key in a_dictionary:
         a_dictionary[key] = value
     else:
-        a_dictionary.append = value
+        a_dictionary[key] = value
     return a_dictionary
