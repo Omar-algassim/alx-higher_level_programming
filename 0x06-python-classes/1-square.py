e3686d9f6a52526e65d41154a20f73a1e20735fd
Omar-algassim <omarggg2@gmail.com>
2023-12-19 16:35:46 +0200

    update

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,11 +1,14 @@
 #!/usr/bin/python3
-#!/usr/bin/python3
 
 """Define a class Square."""
 
 
 class Square:
     """Represent a square."""
+
     pass
-    def __init__(self):
-        self.__size
+
+    def __init__(self, size):
+
+        """Defin the metod of square"""
+        self.__size = size
