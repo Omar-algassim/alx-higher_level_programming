36a020aa6cc28a95c5c3c190f4e063db9ab31865
Omar-algassim <omarggg2@gmail.com>
2023-12-19 16:54:58 +0200

    update

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,1 +1,21 @@
 #!/usr/bin/python3
+"""Define a class Square."""
+
+
+class Square:
+    """Represent a square."""
+
+    pass
+
+    def __init__(self, size=0):
+
+        """Defin the metod of square and most be integer"""
+        self.__size = size
+        try:
+            isinstance(size, int)
+        except TypeError:
+            print("size must be an integer")
+        try:
+            size < 0
+        except ValueError:
+            print("size must be >= 0")
