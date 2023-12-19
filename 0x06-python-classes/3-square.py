0220ffca4398fd7aadfba3e96989684cac19c4c5
Omar-algassim <omarggg2@gmail.com>
2023-12-19 23:02:23 +0200

    update

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,1 +1,21 @@
 #!/usr/bin/python3
+"""Define a class Square."""
+
+
+class Square:
+    """Represent a square."""
+
+    pass
+
+    def __init__(self, size=0):
+
+        """Defin the metod of square and most be integer"""
+
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+    def area(self):
+        return self.__size ** 2
+ 
