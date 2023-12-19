0f9aa0c3d262b4d28545b158f561ef22f0c9ca75
Omar-algassim <omarggg2@gmail.com>
2023-12-19 23:05:05 +0200

    style update

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -16,6 +16,9 @@ class Square:
         if size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
+
     def area(self):
+
+        """return the squre area"""
+
         return self.__size ** 2
- 
