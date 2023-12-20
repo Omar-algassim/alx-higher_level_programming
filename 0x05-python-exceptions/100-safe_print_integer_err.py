54aa1ca4748b75cd0690f73d9d99d83236b1a07f
Omar-algassim <omarggg2@gmail.com>
2023-12-20 18:43:16 +0200

    update

---
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def safe_print_integer_err(value):
+    import sys
+    try:
+        print("{:d}".format(value))
+        return True
+    except (TypeError, ValueError) as e:
+        sys.stderr.write("e\n")
+        return False
