567940a4e85f26145545cf836cb0647a7afcb88e
Omar-algassim <omarggg2@gmail.com>
2024-01-05 03:25:51 +0200

    update

--- 0x08-python-more_classes/4-rectangle.py
+++ 0x08-python-more_classes/4-rectangle.py
@@ -1,1 +1,63 @@
 #!/usr/bin/python3
+"""define rectangle"""
+
+
+class Rectangle:
+
+    """ the rectangle """
+
+    def __init__(self, width=0, height=0):
+
+        """ width and height """
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+
+        self.__width = value
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+
+        """ retutrn area of tri angle """
+        ret = self.__width * self.__height
+        return ret
+
+    def perimeter(self):
+
+        """ returtn the perimeter of triangle """
+        if self.__width == 0 or self.__height == 0:
+            ret = 0
+        else:
+            ret = (self.__width * 2) + (self.__height * 2)
+        return ret
+
+    def __str__(self):
+        if self.__width == 0 or self.__height == 0:
+            return ("")
+        for i in range (self.__height):
+            for j in range (self.__width):
+                print("#", end="")
+            print()
+
+        
