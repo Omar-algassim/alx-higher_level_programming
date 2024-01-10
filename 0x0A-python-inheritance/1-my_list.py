3867223c5b44ce7496137ca19883d4ea3f934495
Omar-algassim <omarggg2@gmail.com>
2024-01-10 21:00:22 +0200

    upload

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,1 +1,13 @@
 #!/usr/bin/python3
+""" define the MyList """
+
+
+class MyList(list):
+    """ subclass from list"""
+    def __init__(self):
+        """ initialze the object """
+        super().__init__()
+    def print_sorted(self):
+        """ print the sorted """
+        print(sorted(self))
+
