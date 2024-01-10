57d5d82ebc4e83add3bfe2ebb2bc3223729c04ff
Omar-algassim <omarggg2@gmail.com>
2024-01-11 01:12:28 +0200

    update

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -1,1 +1,9 @@
 #!/usr/bin/python3
+""" define the same class of object"""
+
+
+def is_kind_of_class(obj, a_class):
+    """ return true if kind of class or 
+    inheritance
+    """
+    return isinstance(obj, a_class)
