68865a06e001e4bf81eed5c294a6650f9964251f
Omar-algassim <omarggg2@gmail.com>
2024-01-11 01:24:40 +0200

    update

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -6,7 +6,7 @@ def inherits_from(obj, a_class):
     """ return true if object is directly or indirectly
     inherite from class
     """
-    if issubclass(type(obj), a_class):
+    if issubclass(type(obj), a_class) and type(obj) != a_class:
         return True
     else:
         return False
