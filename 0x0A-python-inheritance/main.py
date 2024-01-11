e3dbd47967fadfb5f861cf7dcdec8411e13d4958
Omar-algassim <omarggg2@gmail.com>
2024-01-11 03:24:28 +0200

    update

--- 0x0A-python-inheritance/main.py
+++ 0x0A-python-inheritance/main.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
-Square = __import__('10-square').Square
+Square = __import__('11-square').Square
 
-r = Square(13)
+s = Square(13)
 
-print(r)
-print(r.area())
+print(s)
+print(s.area())
+
