414731e1e78ea84a6c16f5def820253bbbba2799
Omar-algassim <omarggg2@gmail.com>
2024-01-11 03:20:04 +0200

    update

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -1,1 +1,13 @@
 #!/usr/bin/python3
+"""define rectangle subclass squre"""
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """squre class froem Rectangle"""
+
+    def __init__(self, size):
+        """ size of squre"""
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size
