341eed044df65166da3389b48f078f8e8a1f2936
Omar-algassim <omarggg2@gmail.com>
2024-01-12 13:24:22 -0800

    update

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -1,1 +1,22 @@
 #!/usr/bin/python3
+"""define matrix divided by number """
+
+
+def matrix_divided(matrix, div):
+    if type(matrix) != list and all(isinstance(row, list)):
+        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+    for row in matrix:
+        if len(row) != len(matrix[0]):
+            raise TypeError("Each row of the matrix must have the same size")
+    if not isinstance(div, int) and not isinstance(div, float):
+        raise TypeError("div must be a number")
+    if div == 0:
+        raise ZeroDivisionError("division by zero")
+    ret = []
+    for row in matrix:
+        re = []
+        for i in row:
+            r = round(i / div, 2)
+            re.append(r)
+        ret.append(re)
+    return ret
