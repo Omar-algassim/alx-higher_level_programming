1d94ec58bcc7a663a99912907ea10b2463c925c1
Omar-algassim <omarggg2@gmail.com>
2024-01-14 02:20:57 +0200

    update

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+def read_file(filename=""):
+    """read file and print in stdout
+
+    Args:
+        filename (str): the name of file. Defaults to "".
+    """
+    with open(filename, encoding="utf-8") as f:
+        data = f.read()
+        print(data)
