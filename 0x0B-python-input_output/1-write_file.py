5d824e1a316857d67bda648555079058c005dad8
Omar-algassim <omarggg2@gmail.com>
2024-01-14 02:41:58 +0200

    update

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,1 +1,10 @@
 #!/usr/bin/python3
+""" define writing in file """
+
+
+def write_file(filename="", text=""):
+    """ write in file and creat it if doesnt exist"""
+    with open(filename, 'w', encoding='utf-8') as f:
+        return f.write(text)
+   
+        
