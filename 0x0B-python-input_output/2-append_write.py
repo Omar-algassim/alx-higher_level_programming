6d3a926910e3e236f357f840c9bf9c41f4e77bfe
Omar-algassim <omarggg2@gmail.com>
2024-01-14 03:10:34 +0200

    update

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -1,1 +1,8 @@
 #!/usr/bin/python3
+""" define append in file """
+
+
+def append_write(filename="", text=""):
+    """ append text in file and creat it if doesnt exist"""
+    with open(filename, 'a', encoding='utf-8') as f:
+        return f.write(text)
