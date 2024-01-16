085eb4a00015dde34824167a6aa622e022c74a64
Omar-algassim <omarggg2@gmail.com>
2024-01-16 15:53:56 +0200

    update

--- 0x0C-python-almost_a_circle/tests/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_base.py
@@ -9,14 +9,23 @@ from models.square import Square
 class testBase(unittest.TestCase):
     """Unittests for testing instantiation of the Base class."""
     
+    def test_type_of_Base(self):
+        """test if is class or not"""
+        obj = Base()
+        self.assertIsInstance(obj, Base)
+
     def test_id(self):
         """test if id increas if it is None"""
         b1 = Base()
         self.assertEqual(b1.id, 1)
         b2 = Base(10)
         self.assertEqual(b2.id, 10)
-        b3 = Base(10)
-        self.assertIsInstance(b3.id, int)
+        b3 = Base(-10)
+        self.assertEqual(b3.id, -10)
+        b4 = Base(0)
+        self.assertEqual(b4.id, 0)
+        b5 = Base(10)
+        self.assertIsInstance(b5.id, int)
     
     def test_to_json_string(self):
         """test for json string method"""
@@ -37,7 +46,9 @@ class testBase(unittest.TestCase):
         """test from_json_string method"""
         json_str = '[{"x": 2, "y":3}, {"o": 5, "p": 9}]'
         py_list = json.loads(json_str)
-        self.assertIsInstance(py_list, list)
-        
-        
-            
+        py_list1 = Base.from_json_string(json_str)
+        self.assertEqual(py_list, py_list1)
+        json_str = None
+        py_list = Base.from_json_string(json_str)
+        self.assertEqual(py_list, [])
+    
