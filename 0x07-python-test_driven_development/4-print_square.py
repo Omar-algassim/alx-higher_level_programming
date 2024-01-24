add34e92d4ef65b98cab2a36d576f4bc587fc1c4
Omar-algassim <omarggg2@gmail.com>
2024-01-24 16:50:08 +0200

    update

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -1,1 +1,15 @@
 #!/usr/bin/python3
+"""define print square"""
+
+
+def print_square(size):
+    if not isinstance(size, int):
+        raise TypeError ("size must be an integer")
+    if size < 0:
+        raise ValueError("size must be >= 0")
+    if isinstance(size, float) or size < 0:
+        raise TypeError("size must be an integer")
+    for i in range(size):
+        for j in range(size):
+            print("#", end="")
+        print()
