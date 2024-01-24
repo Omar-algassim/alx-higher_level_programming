22ce976c3538ae7d57c81721233b2647a56a2e5d
Omar-algassim <omarggg2@gmail.com>
2024-01-24 17:13:48 +0200

    update

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -1,1 +1,18 @@
 #!/usr/bin/python3
+"""define the indention function"""
+
+
+def text_indentation(text):
+    """indentation for string
+
+    Args:
+        text (str): the text will indent
+    """
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
+    print_text = ""
+    for i in text:
+        print_text += i
+        if i == ":" or i == "." or i == "?":
+            print("{}\n".format(print_text))
+            print_text = ""
