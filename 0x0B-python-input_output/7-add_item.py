4abeef79712d412dcdef8676849bea58c8f16123
Omar-algassim <omarggg2@gmail.com>
2024-01-29 19:51:56 +0200

    updat

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
-"""define add item"""
-import json
+""" my add item this module load from file and save and add items from
+    argumennt"""
 import sys
+
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+
 args = load_from_json_file("add_item.json")
 args += sys.argv[1:]
 save_to_json_file(args, "add_item.json")
