77c0bf7e20bb333a9982a07226b3da43c274931a
Omar-algassim <omarggg2@gmail.com>
2024-01-30 00:09:00 +0200

    update

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -1,1 +1,11 @@
 #!/usr/bin/python3
+"""define class to json"""
+
+
+def class_to_json(obj):
+    """return a dictntoiinry
+
+    Args:
+        obj (instance): is an instance of a Class
+    """
+    return obj.__dict__
