5e31ef83ce4ed282fce1223626a28dcdf49cc021
Omar-algassim <omarggg2@gmail.com>
2024-01-30 00:19:58 +0200

    update

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -1,1 +1,22 @@
 #!/usr/bin/python3
+"""define Student class"""
+
+
+class Student:
+    """student calss"""
+    
+    def __init__(self, first_name, last_name, age):
+        """initilize new student
+
+        Args:
+            first_name (str): the first name of student 
+            last_name (str): the last name of student
+            age (int): the student age
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+        
+    def to_json(self):
+        """class to json"""
+        return self.__dict__
