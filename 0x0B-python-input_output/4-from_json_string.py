152c49286d315c7fd91756ec2651e371db789bac
Omar-algassim <omarggg2@gmail.com>
2024-01-29 16:02:21 +0200

    update

--- 0x0B-python-input_output/4-from_json_string.py
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -1,1 +1,11 @@
 #!/usr/bin/python3
+""" load json file """
+import json
+
+def from_json_string(my_str):
+    """load json to python
+
+    Args:
+        my_str (str): string in json
+    """
+    return json.loads(my_str)
