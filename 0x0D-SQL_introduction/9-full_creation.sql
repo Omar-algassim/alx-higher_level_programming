a7961fbb52b6339faa0873b26b758813a7388fce
Omar-Algassim <omarggg2@gamil.com>
2024-03-03 20:16:57 +0200

    update

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,4 +1,11 @@
 -- create second table and give the value
+ CREATE TABLE IF NOT EXISTS second_table (id INT, name VARCHAR(256), score INT);
+
+INSERT INTO second_table
+VALUES (1, "John", 10),
+(2, "Alex", 3),
+(3, "Bob", 14),
+(4, "George", 8);-- create second table and give the value
  CREATE TABLE second_table(
     id INT,
     name VACHAR(256),
