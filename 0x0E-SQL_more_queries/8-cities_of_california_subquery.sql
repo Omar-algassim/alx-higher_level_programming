204b62fdac40c66e45d9b8ac91afaff6eb18bb15
Omar-Algassim <omarggg2@gamil.com>
2024-03-05 22:39:05 +0200

    update

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,2 +1,2 @@
 --  lists all the cities of California that can be found in the database hbtn_0d_usa.
-SELECT cities.id, cities.name WHERE state.name = 'California';
+SELECT id, name from cities WHERE state_id IN (SELECT id FROM states WHERE name = 'California');
