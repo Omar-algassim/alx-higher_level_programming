e57a28213e88f11a80dfdac6497851d20682dbaf
Omar-Algassim <omarggg2@gamil.com>
2024-03-06 02:49:39 +0200

    update

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -4,4 +4,4 @@ FROM tv_shows
 JOIN tv_show_genres ON tv_show_genres.show_id = tv_shows.id
 JOIN tv_genres ON tv_genres.id = tv_show_genres.genre_id 
 GROUP BY tv_genres.name 
-ORDER BY "numbe_of_shows" DESC;
+ORDER BY number_of_shows DESC;
