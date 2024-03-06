0b2cf30fe5670341078dd55901550e85f543a56a
Omar-Algassim <omarggg2@gamil.com>
2024-03-06 04:38:39 +0200

    update

--- 0x0E-SQL_more_queries/102-rating_shows.sql
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -1,8 +1,6 @@
 -- Write a script that lists all shows from hbtn_0d_tvshows_rate by their rating.
-SELECT tv_genres.name, SUM(tv_show_ratings.rate) AS "rating"
-FROM tv_shows
-JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
-JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
+SELECT tv_shows.title, SUM(tv_show_ratings.rate) AS "rating" FROM tv_shows 
 JOIN tv_show_ratings ON tv_shows.id = tv_show_ratings.show_id
-GROUP BY tv_genres.name
+JOIN tv_genres ON tv_genres.id = tv_show_ratings.genre_id
+GROUP BY tv_shows.title
 ORDER BY rating DESC;
