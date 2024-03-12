06a6a2bbf66cadab38db834f67af2c4e57423757
Omar-Algassim <omarggg2@gamil.com>
2024-03-12 04:38:50 +0200

    update

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,1 +1,10 @@
 #!/usr/bin/node
+function factrial (num) {
+  if (isNaN(num) || num === 1) {
+    return (1);
+  }
+  return (num * factrial(num - 1));
+}
+const num = parseInt(process.argv[2]);
+const result = factrial(num);
+console.log(result);
