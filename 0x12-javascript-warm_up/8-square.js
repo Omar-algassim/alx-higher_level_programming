23c4a34db2c48f83d44400c67fcab8b6dd6475da
Omar-Algassim <omarggg2@gamil.com>
2024-03-12 04:13:28 +0200

    update

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,1 +1,15 @@
 #!/usr/bin/node
+let i = 0;
+const size = parseInt(process.argv[2]);
+if (isNaN(size)) {
+  console.log('Missing size');
+} else {
+  while (i < size) {
+    let print = '';
+    for (let j = 0; j < size; j++) {
+      print += 'X';
+    }
+    console.log(print);
+    i++;
+  }
+}
