990a6c5e3be70faa640c136b4cf6b21a9f66e305
Omar-Algassim <omarggg2@gamil.com>
2024-03-12 19:40:47 +0200

    !== semistandard problem changed

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 class Rectangle {
   constructor (w, h) {
-    if ((w != undefined && w > 0) && (h != undefined && h > 0)) {
+    if ((w !== undefined && w > 0) && (h !== undefined && h > 0)) {
       this.width = w;
       this.height = h;
     }
