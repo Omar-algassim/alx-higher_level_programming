2636a917f055b00aca81570b4104175e8072a5f3
Omar-Algassim <omarggg2@gamil.com>
2024-03-13 04:21:11 +0200

    update

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -1,1 +1,10 @@
 #!/usr/bin/node
+exports.nbOccurences = function (list, searchElement) {
+  let count = 0;
+  for (let index = 0; index < list.length; index++) {
+    if (searchElement === list[index]) {
+      count++;
+    }
+  }
+  return count;
+};
