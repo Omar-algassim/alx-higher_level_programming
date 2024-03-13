92d34fa93aa47d05bd35a23621cad5cfc8042a08
Omar-Algassim <omarggg2@gamil.com>
2024-03-13 04:07:04 +0200

    updatee

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,1 +1,22 @@
 #!/usr/bin/node
+const Square_ = require('./5-square');
+
+class Square extends Square_ {
+    constructor (size) {
+        super(size);
+    }
+    charPrint(c) {
+        if (c === 'C') {
+            for (let i = 0; i < this.height; i++) {
+                let row = '';
+                for (let j = 0; j < this.height; j++) {
+                  row += 'C';
+                }
+                console.log(row);
+        }
+    } else {
+        super.print();
+    }
+}
+}
+module.exports = Square;
