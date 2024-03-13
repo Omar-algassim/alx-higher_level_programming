7c6d5251bc5cf282b50703a3a81c6365d7c51aac
Omar-Algassim <omarggg2@gamil.com>
2024-03-13 17:05:07 +0200

    update

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,16 +1,17 @@
 #!/usr/bin/node
-  let array = process.argv;
-  if (array[2] === undefined || array.length < 4) {
-    console.log(0)
-  } else {
-    let max = array[2];
-    let secondMax = array[3];
-    for (let index = 2; index < array.length; index++) {
-     if (max < array[index]) {
+const array = process.argv;
+if (array[2] === undefined || array.length < 4) {
+  console.log(0);
+} else {
+  let max = array[2];
+  let secondMax = array[3];
+  for (let index = 2; index < array.length; index++) {
+    if (max < array[index]) {
       secondMax = max;
       max = array[index];
-     }
-      
+    } else if (secondMax < array[index] && max > array[index]) {
+      secondMax = array[index];
     }
-    console.log(secondMax);
   }
+  console.log(secondMax);
+}
