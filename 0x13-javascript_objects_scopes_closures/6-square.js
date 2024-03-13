d9467909b01061242a85f5905ea83b03e8f81ea8
Omar-Algassim <omarggg2@gamil.com>
2024-03-13 04:12:20 +0200

    print c change

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -2,16 +2,12 @@
 const Square_ = require('./5-square');
 
 class Square extends Square_ {
-  constructor (size) {
-    super(size);
-  }
-
   charPrint (c) {
     if (c) {
       for (let i = 0; i < this.height; i++) {
         let row = '';
         for (let j = 0; j < this.height; j++) {
-          row += 'C';
+          row += 'c';
         }
         console.log(row);
       }
