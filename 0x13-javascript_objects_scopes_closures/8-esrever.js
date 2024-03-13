29890fe133a1779669019d247a6378d62468d33b
Omar-Algassim <omarggg2@gamil.com>
2024-03-13 04:28:55 +0200

    update

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,1 +1,10 @@
 #!/usr/bin/node
+exports.esrever = function (list) {
+  const retList = [];
+  let i = 1;
+  for (let index = 0; index < list.length; index++) {
+    retList[index] = list[list.length - i];
+    i++;
+  }
+  return retList;
+};
