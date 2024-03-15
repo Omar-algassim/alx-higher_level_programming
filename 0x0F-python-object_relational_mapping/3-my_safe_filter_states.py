7f1e66538eac19a7904a8a9a7847e0cedd971b1f
Omar-Algassim <omarggg2@gamil.com>
2024-03-16 00:43:22 +0200

    update

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,1 +1,18 @@
 #!/usr/bin/python3
+"""lists all states from the database hbtn_0e_0_usa:"""
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2],
+                         db=sys.argv[3], host='localhost', port=3306)
+    cur = db.cursor()
+    name = sys.argv[4]
+    cur.execute("SELECT * FROM states WHERE name LIKE BINARY '{}'"
+                .format(sys.argv[4]))
+    state = cur.fetchall()
+    for i in state:
+        print(i)
+
+    cur.close()
+    db.close()
