a77be1692b89270ac6fd0a2eaba9f5ee7a0b872b
Omar-Algassim <omarggg2@gamil.com>
2024-03-15 22:14:03 +0200

    upaet

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -8,7 +8,7 @@ if __name__ == "__main__":
                          db=sys.argv[3], host='localhost', port=3306)
     cur = db.cursor()
     name = sys.argv[4]
-    cur.execute("SELECT * FROM states WHERE name = '{}' ORDER BY id"
+    cur.execute("SELECT * FROM states WHERE name LIKE '{}' ORDER BY id"
                 .format(sys.argv[4]))
     state = cur.fetchall()
     for i in state:
