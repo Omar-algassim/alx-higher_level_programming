7473ca28630e39d9cd607fe8a6b66e6512a74647
Omar-algassim <omarggg2@gmail.com>
2024-03-16 14:53:02 -0700

    make code not execute when imported

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -6,18 +6,18 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 import sys
 
-user = sys.argv[1]
-passwd = sys.argv[2]
-db = sys.argv[3]
 if __name__ == "__main__":
+    user = sys.argv[1]
+    passwd = sys.argv[2]
+    db = sys.argv[3]
     engine = create_engine("mysql+mysqldb://{}:{}@localhost:3306/{}"
                        .format(user, passwd, db))
-Base = declarative_base()
+    Base = declarative_base()
 
 
-class State(Base):
-    """state class
-    """
-    __tablename__ = "states"
-    id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
-    name = Column(String(128), nullable=False)
+    class State(Base):
+        """state class
+        """
+        __tablename__ = "states"
+        id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
+        name = Column(String(128), nullable=False)
