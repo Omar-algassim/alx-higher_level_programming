e3b5b1b90f0563cfc5391709f5b4429103358fa1
Omar-Algassim <omarggg2@gamil.com>
2024-03-16 22:40:57 +0200

    docmentation

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+"""defien class for base model"""
 from sqlalchemy import create_engine
 from sqlalchemy import Integer, String, Column
 from sqlalchemy.orm import sessionmaker
@@ -11,9 +12,12 @@ engine = create_engine("mysql+mysqldb://{}:{}@localhost:3306/{}"
                        .format(user, passwd, db))
 Base = declaritive_base()
 
+
 class State(Base):
+    """state class
+    """
     __tablename__ = "states"
     id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
     name = Column(String(128), nullable=False)
-    
+
 Base.metadata.create_all(engine)
