0fc0401fa409b7e7b24d8ac12c1ad6715a4ed36b
Omar-Algassim <omarggg2@gamil.com>
2024-03-16 22:37:16 +0200

    create base calss

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,1 +1,19 @@
 #!/usr/bin/python3
+from sqlalchemy import create_engine
+from sqlalchemy import Integer, String, Column
+from sqlalchemy.orm import sessionmaker
+import sys
+
+user = sys.argv[1]
+passwd = sys.argv[2]
+db = sys.argv[3]
+engine = create_engine("mysql+mysqldb://{}:{}@localhost:3306/{}"
+                       .format(user, passwd, db))
+Base = declaritive_base()
+
+class State(Base):
+    __tablename__ = "states"
+    id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
+    name = Column(String(128), nullable=False)
+    
+Base.metadata.create_all(engine)
