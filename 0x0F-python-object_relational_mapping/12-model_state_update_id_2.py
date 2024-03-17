8ff8c0eed7261fcc760f558769f0752e6519181e
Omar-Algassim <omarggg2@gamil.com>
2024-03-17 15:34:06 +0200

    update the name withe id = 2 to new maxico

--- 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -1,1 +1,20 @@
 #!/usr/bin/python3
+"""create new column in table with name 'Louisiana' and print the id"""
+
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+import sys
+
+if __name__ == "__main__":
+
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]))
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    data = session.query(State).filter(State.id == 2).update(
+        {State.name: 'New Mexico'})
+    session.commit()
