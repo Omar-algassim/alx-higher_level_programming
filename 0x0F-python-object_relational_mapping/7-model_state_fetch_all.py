ba71b262210bb0077722da546c421eab940130aa
Omar-Algassim <omarggg2@gamil.com>
2024-03-17 04:34:56 +0200

    print all states in table

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,1 +1,15 @@
 #!/usr/bin/python3
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+import sys
+
+engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                       .format(sys.argv[1], sys.argv[2], sys.argv[3]))
+
+Session = sessionmaker(bind=engine)
+session = Session()
+
+data = session.query(State).all()
+for obj in data:
+    print("{}: {}".format(obj.id, obj.name))
