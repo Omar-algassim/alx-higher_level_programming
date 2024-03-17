95faa5e52977f757259785240056595991646dfd
Omar-Algassim <omarggg2@gamil.com>
2024-03-18 01:28:58 +0200

    list all citis in state

--- 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -1,24 +1,25 @@
 #!/usr/bin/python3
-"""lists all State objects, and corresponding City objects,
-contained in the database hbtn_0e_101_usa"""
+"""creates the State “California” with the City “San Francisco” 
+from the database hbtn_0e_100_usa"""
 
 
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from model_state import Base, State
-from model_city import City
+from relationship_state import Base, State
+from relationship_city import City
 import sys
 
 if __name__ == "__main__":
 
     engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
                            .format(sys.argv[1], sys.argv[2], sys.argv[3]))
+    Base.metadata.create_all(engine)
 
     Session = sessionmaker(bind=engine)
     session = Session()
-
-    data = session.query(State, City).join(City).all()
-    data = session.query(State, City).join(City).all()
-    print(type(data[0]))
-     for state in data:
-         print("{}: {}".format(state.id, state.name))
+    
+    data = session.query(State).all()
+    for state in data:
+        print("{}: {}".format(state.id, state.name))
+        for city in state.cities:
+            print("\t{}: {}".format(city.id, city.name))
