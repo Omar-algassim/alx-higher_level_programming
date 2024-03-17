b4b7877e5b65f0183027a1f5668478e9552bb0ca
Omar-Algassim <omarggg2@gamil.com>
2024-03-17 14:58:18 +0200

    print the row contain 'A' in the table

--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -1,1 +1,20 @@
 #!/usr/bin/python3
+"""print the row contain 'A' in the table"""
+
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+import sys
+
+if __name__ == "__main__":
+
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]))
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    data = session.query(State).filter(State.name.like('%A%')).all()
+    for row in data:
+        print("{}: {}".format(row.id, row.name))
