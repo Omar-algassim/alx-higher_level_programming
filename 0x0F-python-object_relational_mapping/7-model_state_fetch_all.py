6ed3606811862c18254ea4a0ff3c120fcde570f6
Omar-algassim <omarggg2@gmail.com>
2024-03-16 21:04:53 -0700

    add docmntation

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+"""list all id and name in states class"""
+
+
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
