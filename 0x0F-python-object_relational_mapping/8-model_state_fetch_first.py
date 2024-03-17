092c3145c56ac6201646058f253792e7b187cc46
Omar-Algassim <omarggg2@gamil.com>
2024-03-17 14:46:35 +0200

    add docmntation

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-    """print the first row in the table
-    """
+"""print the first row in the table"""
+
+
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
