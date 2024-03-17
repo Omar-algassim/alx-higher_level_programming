92d0f7be1f4666eb9db1dcab6345e7a048d00f3a
Omar-Algassim <omarggg2@gamil.com>
2024-03-17 15:09:49 +0200

    print all name id for specific name in the table

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -1,1 +1,22 @@
 #!/usr/bin/python3
+"""print the row contain 'A' in the table"""
+
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+import sys
+
+if __name__ == "__main__":
+
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]))
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    data = session.query(State).filter(State.name == f"{sys.argv[4]}").all()
+    if len(data) < 1:
+        print("Not found")
+    for row in data:
+        print(row.id)
