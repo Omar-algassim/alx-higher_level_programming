bee0a225f7a3b84d17e18adfe7ff0fa56394095f
Omar-Algassim <omarggg2@gamil.com>
2024-03-18 01:09:41 +0200

    relation fix

--- 0x0F-python-object_relational_mapping/relationship_state.py
+++ 0x0F-python-object_relational_mapping/relationship_state.py
@@ -13,4 +13,4 @@ class State(Base):
     __tablename__ = "states"
     id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
     name = Column(String(128), nullable=False)
-    cities = relationship(City, cascade="all, delete", backref=cities)
+    cities = relationship(City, cascade="all, delete", backref="state")
