dcff2f66aad2951097d6db023379d60d4b9f0a73
Omar-Algassim <omarggg2@gamil.com>
2024-03-17 03:07:57 +0200

    update

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -5,11 +5,6 @@ from sqlalchemy import Integer, String, Column
 from sqlalchemy.ext.declarative import declarative_base
 import sys
 
-user = sys.argv[1]
-passwd = sys.argv[2]
-db = sys.argv[3]
-engine = create_engine("mysql+mysqldb://{}:{}@localhost:3306/{}"
-                       .format(user, passwd, db))
 Base = declarative_base()
 
 
