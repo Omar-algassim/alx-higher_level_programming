d89b24efa386a68ec4a6012f18cf37a363ec7424
Omar-Algassim <omarggg2@gamil.com>
2024-03-17 15:21:40 +0200

    create new column in table with name 'Louisiana'

--- 0x0F-python-object_relational_mapping/11-model_state_insert.py
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -1,1 +1,24 @@
 #!/usr/bin/python3
+"""create new column in table with name 'Louisiana' """
+
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+import sys
+
+if __name__ == "__main__":
+
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]))
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    new = State(name='Louisiana')
+    session.add(new)
+    session.commit()
+    
+    data = session.query(State).filter(State.name == 'Louisiana').all()
+    for row in data:
+        print(row.id)
