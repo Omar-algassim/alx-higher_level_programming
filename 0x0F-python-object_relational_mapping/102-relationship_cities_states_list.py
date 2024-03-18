f44613d37abeeb728de36fc251f2d393c9df1e49
Omar-Algassim <omarggg2@gamil.com>
2024-03-18 02:02:30 +0200

    updtae

--- 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
+++ 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""creates the State “California” with the City “San Francisco” 
+"""creates the State “California” with the City “San Francisco”
 from the database hbtn_0e_100_usa"""
 
 
@@ -13,15 +13,10 @@ if __name__ == "__main__":
 
     engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
                            .format(sys.argv[1], sys.argv[2], sys.argv[3]))
-    Base.metadata.create_all(engine, pool_pre_ping=True)
 
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    new_state = State(name='California')
-    new_city = City(name='San Francisco')
-    new_state.cities.append(new_city)
-
-    session.add(new_state)
-    session.add(new_city)
-    session.commit()
+    data = session.query(City).all()
+    for city in data:
+        print("{}: {} -> {}".format(city.id, city.name, city.state.name))
