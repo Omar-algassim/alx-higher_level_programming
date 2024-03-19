21a52ba8a8a10c611f7f59dd77c11d341f868bd3
Omar-Algassim <omarggg2@gamil.com>
2024-03-19 21:14:13 +0200

    pycodestyle fix

--- 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
+++ 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""creates the State “California” with the City “San Francisco” 
+"""creates the State “California” with the City “San Francisco”
 from the database hbtn_0e_100_usa"""
 
 
