7e029b5ca386f1071810994be031acdde4ca1736
Omar-Algassim <omarggg2@gamil.com>
2024-03-28 18:12:18 +0200

    post email

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,1 +1,15 @@
 #!/usr/bin/python3
+""" post header"""
+
+
+import urllib.request
+import urllib.parse
+import sys
+
+url = sys.argv[1]
+email = sys.argv[2]
+data_dict = {"email": email}
+data = urllib.parse.urlencode(data_dict)
+data = data.encode('utf8')
+with urllib.request.urlopen(url, data) as response:
+    print(response.read().decode('utf8'))
