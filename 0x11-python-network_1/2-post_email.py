8627eda8b0bf4abeeac0bce4edda372984690a71
Omar-Algassim <omarggg2@gamil.com>
2024-03-28 19:20:48 +0200

    add if __name__ == '__main__':

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -4,11 +4,11 @@
 
 from urllib import request, parse
 import sys
-
-url = sys.argv[1]
-email = sys.argv[2]
-data_dict = {"email": email}
-data = parse.urlencode(data_dict)
-data = data.encode('utf8')
-with request.urlopen(url, data) as response:
-    print(response.read().decode('utf8'))
+if __name__ == '__main__':
+    url = sys.argv[1]
+    email = sys.argv[2]
+    data_dict = {"email": email}
+    data = parse.urlencode(data_dict)
+    data = data.encode('utf8')
+    with request.urlopen(url, data) as response:
+        print(response.read().decode('utf8'))
