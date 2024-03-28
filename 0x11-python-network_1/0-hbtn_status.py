6fd8b9af4079fa8e57c36d88d216d417b1297896
Omar-Algassim <omarggg2@gamil.com>
2024-03-28 17:07:56 +0200

    capitlization fix

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -7,7 +7,7 @@ import urllib.request
 if __name__ == "__main__":
     with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as r:
         content = r.read()
-        print('body respons:')
+        print('Body response:')
         print(f"\t- type: {type(content)}")
         print(f"\t- content: {content}")
         print(f"\t- utf8 content: {content.decode('utf8')}")
