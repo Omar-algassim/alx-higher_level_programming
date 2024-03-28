4a77f3970d7b2111e7990d7a80789fe373423372
Omar-Algassim <omarggg2@gamil.com>
2024-03-28 17:06:27 +0200

    fetch url https://alx-intranet.hbtn.io/status

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -5,9 +5,9 @@
 import urllib.request
 
 if __name__ == "__main__":
-    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as respons:
+    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as r:
+        content = r.read()
         print('body respons:')
-        print(f"\t- type: {type(respons.read())}")
-        print(f"\t- content: {respons.read()}")
-        print(f"\t- utf8 content: {respons.read()}")
-        
+        print(f"\t- type: {type(content)}")
+        print(f"\t- content: {content}")
+        print(f"\t- utf8 content: {content.decode('utf8')}")
