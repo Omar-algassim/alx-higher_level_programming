532206c040bc05f416aa04f1f5d8e7e14c6df73e
Omar-Algassim <omarggg2@gamil.com>
2024-03-28 18:32:44 +0200

    chnge import format

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -2,14 +2,13 @@
 """ post header"""
 
 
-import urllib.request
-import urllib.parse
+from urllib import request, parse
 import sys
 
 url = sys.argv[1]
 email = sys.argv[2]
 data_dict = {"email": email}
-data = urllib.parse.urlencode(data_dict)
+data = parse.urlencode(data_dict)
 data = data.encode('utf8')
-with urllib.request.urlopen(url, data) as response:
+with request.urlopen(url, data) as response:
     print(response.read().decode('utf8'))
