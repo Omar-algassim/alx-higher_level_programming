a7a38916d131107843d8bb94459efde4ebc0556f
Omar-Algassim <omarggg2@gamil.com>
2024-03-29 16:26:31 +0200

    delete "with"

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -2,11 +2,10 @@
 """retrive the id of the request"""
 
 
-
 import requests
 import sys
 
 url = sys.argv[1]
 if "__main__" == __name__:
-    with requests.get(url) as web:
-        print(web.headers.get('X-Request-Id'))
+    web = requests.get(url)
+    print(web.headers.get('X-Request-Id'))
