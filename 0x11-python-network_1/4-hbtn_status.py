88d8be33de1035d67500af2d0e8f2f86b2a3d008
Omar-Algassim <omarggg2@gamil.com>
2024-03-29 05:11:40 +0200

    get request

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,1 +1,12 @@
 #!/usr/bin/python3
+""" get request """
+
+
+import requests
+
+if "__main__" == __name__:
+    url = "https://alx-intranet.hbtn.io/status"
+    response = requests.get(url)
+    print("Body response:")
+    print(f"\t- type: {type(response.content.decode('utf8'))}")
+    print(f"\t- content: {response.content.decode('utf8')}")
