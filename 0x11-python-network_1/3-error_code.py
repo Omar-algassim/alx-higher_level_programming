0492111a331fac6090654a68e3008065171961ef
Omar-Algassim <omarggg2@gamil.com>
2024-03-29 04:35:47 +0200

    handle error

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,1 +1,14 @@
 #!/usr/bin/python3
+""" handel 404 error """
+
+
+from urllib import request, error
+import sys
+
+url = sys.argv[1]
+
+try:
+    response = request.urlopen(url)
+    print(response)
+except error.HTTPError as e:
+    print(f"Error code: {e.code}")
