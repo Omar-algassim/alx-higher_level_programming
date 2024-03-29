ab3296670f17635ab26ecbb0c1543af91283f8d0
Omar-Algassim <omarggg2@gamil.com>
2024-03-29 16:21:03 +0200

    get request id

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -1,1 +1,12 @@
 #!/usr/bin/python3
+""" retrive the info of request"""
+
+
+import requests
+import sys
+
+
+url = sys.argv[1]
+if "__main__" == __name__:
+    with requests.get(url) as web:
+        print(web.headers.get('X-Request-Id'))
