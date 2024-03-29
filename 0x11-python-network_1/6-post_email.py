470a791b5bcd8e5d64678396eeffb2e60d73c850
Omar-Algassim <omarggg2@gamil.com>
2024-03-29 16:32:37 +0200

    post request

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,1 +1,16 @@
 #!/usr/bin/python3
+"""rtakes in a URL and an email address, sends a POST request to
+the passed URL with the email
+as a parameter, and finally displays the body of the response."""
+
+
+import requests
+import sys
+
+if "__main__" == __name__:
+    url = sys.argv[1]
+    email = sys.argv[2]
+    data = {"email": email}
+    requests.post(url=url, data=data)
+    web = requests.get(url)
+    web.headers.get('email')
