5c476a5ab8ccba95cf71e6809db1e0e946ecb4d5
Omar-Algassim <omarggg2@gamil.com>
2024-03-29 04:37:42 +0200

    handle error

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -4,11 +4,11 @@
 
 from urllib import request, error
 import sys
+if __name__ == '__main__':
+    url = sys.argv[1]
 
-url = sys.argv[1]
-
-try:
-    response = request.urlopen(url)
-    print(response)
-except error.HTTPError as e:
-    print(f"Error code: {e.code}")
+    try:
+        with request.urlopen(url) as response:
+            print(response.read().decode('utf8'))
+    except error.HTTPError as e:
+        print(f"Error code: {e.code}")
