82019cdf2916d6702ada9c3a195be0170518aceb
Omar-Algassim <omarggg2@gamil.com>
2024-04-24 01:45:15 +0200

    semistandr fix

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -2,11 +2,11 @@
 
 const fs = require('fs');
 
-const file = process.argv[2]
-fs.readFile(file, "utf8", (err, data) => {
-    if (err){
-        console.error(err);
-        return;
-    }
-    console.log(data)
+const file = process.argv[2];
+fs.readFile(file, 'utf8', (err, data) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+  console.log(data);
 });
